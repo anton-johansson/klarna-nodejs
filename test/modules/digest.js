cdc9598056d36c02a21bed4e42bd4de4a5704525
anton-johansson <antoon.johansson@gmail.com>
2015-02-03 18:56:55 +0100

    Added testing framework

--- test/modules/digest.js
+++ test/modules/digest.js
@@ -1,9 +1,21 @@
+var assert = require('assert');
 var digest = require('../../modules/digest');
 
-var expected = 'apG3SvlfE1zYCcZ+ka0w5/FDGzA9cZwMjbGC+WkDjl8=';
-var actual = digest([ 123, '410321-9202', 'secret' ]);
+describe('digest.js', function()
+{
+    describe('#digest(arguments)', function()
+    {
+        it('should return proper digested string', function()
+        {
+            var expected = 'apG3SvlfE1zYCcZ+ka0w5/FDGzA9cZwMjbGC+WkDjl8=';
+            var actual = digest([ 123, '410321-9202', 'secret' ]);
 
-console.log(actual);
+            assert.equal(expected, actual);
+        });
 
-// assertEquals(expected, actual);
-
+        it('should throw error if arguments is not an array', function()
+        {
+            assert.equal(false, true);
+        });
+    });
+});
