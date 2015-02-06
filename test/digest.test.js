dfc27c8b3157bf709952c9f658db55ee0c6accfc
anton-johansson <antoon.johansson@gmail.com>
2015-02-06 20:05:29 +0100

    Added test for fetching company addresses

--- test/digest.test.js
+++ test/digest.test.js
@@ -5,19 +5,21 @@ describe('digest.js', function ()
 {
 	describe('#digest(arguments)', function ()
 	{
-		it('should return proper digested string', function ()
+		it('should return proper digested string', function (done)
 		{
 			assertDigest([123, '410321-9202', 'secret'], 'apG3SvlfE1zYCcZ+ka0w5/FDGzA9cZwMjbGC+WkDjl8=');
 			assertDigest([666, '002031-0132', 'some-secret-password'], 'F19bBW5J+wPNRJ66hQdE0GpQEoSbUMk6o0+Mv+TpFaE=');
 			assertDigest([], '47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=');
+			done();
 		});
 
-		it('should throw error if arguments is not an array', function ()
+		it('should throw error if arguments is not an array', function (done)
 		{
 			expect(function () { digest(undefined)		}).toThrow("'arguments' must be an array");
 			expect(function () { digest('hej')			}).toThrow("'arguments' must be an array");
 			expect(function () { digest(123)			}).toThrow("'arguments' must be an array");
 			expect(function () { digest([])				}).toNotThrow();
+			done();
 		});
 	});
 });
--- test/klarna.test.js
+++ test/klarna.test.js
@@ -52,7 +52,7 @@ describe('klarna.js', function()
 {
 	describe('#getAddresses(number, success, failure)', function ()
 	{
-		it('should return addresses for a person properly', function()
+		it('should return addresses for a person properly', function(done)
 		{
 			klarna.getAddresses('410321-9202', function(actual)
 			{
@@ -70,9 +70,44 @@ describe('klarna.js', function()
 				];
 
 				assert.deepEqual(expected, actual);
+				done();
 			}, function(error)
 			{
 				assert.fail('', '', 'Expected success, but got an error.');
+				done();
+			});
+		});
+
+		it('should return addresses for a company properly', function(done)
+		{
+			klarna.getAddresses('002031-0132', function(actual)
+			{
+				var expected =
+				[
+					{
+						isCompany: true,
+						companyName: 'Testcompany-se',
+						street: 'Stårgatan 1',
+						zipCode: '12345',
+						city: 'Ankeborg',
+						country: '209'
+					},
+					{
+						isCompany: true,
+						companyName: 'Testcompany-se',
+						street: 'lillegatan 1',
+						zipCode: '12334',
+						city: 'Ankeborg',
+						country: '209'
+					}
+				];
+
+				assert.deepEqual(expected, actual);
+				done();
+			}, function(error)
+			{
+				assert.fail('', '', 'Expected success, but got an error.');
+				done();
 			});
 		});
 	});
